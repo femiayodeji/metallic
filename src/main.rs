ee522216ae5e4bf12fc48d2c13ff0017eb0b62fb
feminstincts <feminstincts@gmail.com>
2019-04-23 22:36:42 +0100

    a minimal rust kernel

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,22 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hi Instincts!";
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+	let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
 
-/// This function is called on panic.
+// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
