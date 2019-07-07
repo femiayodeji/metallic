6ac368aaa18b0df0f613e80cc2508f31b46b8efa
feminstincts <feminstincts@gmail.com>
2019-07-07 10:17:02 +0100

    added vga text mode

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,18 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hi Instincts!";
+mod vga_buffer;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-	let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+	println!("Welcome{} to {}", "!", "metallic");
+    
     loop {}
 }
 
 // This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
