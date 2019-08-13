ee8d9bbd29c48325d51dc6c39f67e4faf9fe9421
feminstincts <feminstincts@gmail.com>
2019-08-13 03:36:32 +0100

    add unit test

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-use core::panic::PanicInfo;
+//test
+![feature(custom_test_frameworks)]
+#![test_runner(metallic::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
+use metallic::println;
+use core::panic::PanicInfo;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
 	println!("Welcome{} to {}", "!", "metallic");    
 
+
+	#[cfg(test)]
+	test_main();
+
+
     loop {}
 }
 
-// This function is called on panic.
+
+/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-//test
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    metallic::test_panic_handler(info)
 }
